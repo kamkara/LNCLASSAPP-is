d8b54764052ff56484dae0e3ac1b7e416dd6304b
kamkara <lnclass.kamkara@gmail.com>
2021-09-02 17:23:19 +0000

    add slug feature

--- Gemfile
+++ Gemfile
@@ -28,6 +28,17 @@ gem 'jbuilder', '~> 2.7'
 # Reduces boot times through caching; required in config/boot.rb
 gem 'bootsnap', '>= 1.4.4', require: false
 
+
+
+
+########## STYLES ##########
+gem 'bootstrap', '~> 5.1'
+
+
+########## SLUG ##########
+gem 'friendly_id', '~> 5.4', '>= 5.4.2'
+
+
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
   gem 'byebug', platforms: [:mri, :mingw, :x64_mingw]
@@ -43,6 +54,8 @@ group :development do
   # Spring speeds up development by keeping your application running in the background. Read more: https://github.com/rails/spring
   gem 'spring'
 end
+
+
 
 group :test do
   # Adds support for Capybara system testing and selenium driver
--- db/schema.rb
+++ db/schema.rb
@@ -10,35 +10,11 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_09_02_171023) do
+ActiveRecord::Schema.define(version: 2021_09_02_171629) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "pgcrypto"
   enable_extension "plpgsql"
-
-  create_table "action_text_rich_texts", force: :cascade do |t|
-    t.string "name", null: false
-    t.text "body"
-    t.string "record_type", null: false
-    t.bigint "record_id", null: false
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-    t.index ["record_type", "record_id", "name"], name: "index_action_text_rich_texts_uniqueness", unique: true
-  end
-
-  create_table "courses", id: :uuid, default: -> { "gen_random_uuid()" }, force: :cascade do |t|
-    t.string "title"
-    t.text "content"
-    t.string "slug"
-    t.uuid "level_id", null: false
-    t.uuid "material_id", null: false
-    t.uuid "user_id", null: false
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-    t.index ["level_id"], name: "index_courses_on_level_id"
-    t.index ["material_id"], name: "index_courses_on_material_id"
-    t.index ["user_id"], name: "index_courses_on_user_id"
-  end
 
   create_table "friendly_id_slugs", force: :cascade do |t|
     t.string "slug", null: false
@@ -51,59 +27,4 @@ ActiveRecord::Schema.define(version: 2021_09_02_171023) do
     t.index ["sluggable_type", "sluggable_id"], name: "index_friendly_id_slugs_on_sluggable_type_and_sluggable_id"
   end
 
-  create_table "levels", id: :uuid, default: -> { "gen_random_uuid()" }, force: :cascade do |t|
-    t.string "title"
-    t.string "slug"
-    t.uuid "user_id", null: false
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-    t.index ["slug"], name: "index_levels_on_slug", unique: true
-    t.index ["user_id"], name: "index_levels_on_user_id"
-  end
-
-  create_table "materials", id: :uuid, default: -> { "gen_random_uuid()" }, force: :cascade do |t|
-    t.string "title"
-    t.string "slug"
-    t.uuid "user_id", null: false
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-    t.index ["slug"], name: "index_materials_on_slug", unique: true
-    t.index ["user_id"], name: "index_materials_on_user_id"
-  end
-
-  create_table "users", id: :uuid, default: -> { "gen_random_uuid()" }, force: :cascade do |t|
-    t.string "email", default: "", null: false
-    t.string "encrypted_password", default: "", null: false
-    t.string "reset_password_token"
-    t.datetime "reset_password_sent_at"
-    t.datetime "remember_created_at"
-    t.integer "sign_in_count", default: 0, null: false
-    t.datetime "current_sign_in_at"
-    t.datetime "last_sign_in_at"
-    t.string "current_sign_in_ip"
-    t.string "last_sign_in_ip"
-    t.string "first_name"
-    t.string "last_name"
-    t.string "full_name"
-    t.string "matricule", default: "", null: false
-    t.string "city"
-    t.string "school_name"
-    t.string "status_bac"
-    t.string "serie_bac"
-    t.string "gender"
-    t.string "contact", default: "", null: false
-    t.string "role", default: "student"
-    t.string "ref_payment"
-    t.string "slug"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-    t.index ["email"], name: "index_users_on_email", unique: true
-    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
-  end
-
-  add_foreign_key "courses", "levels"
-  add_foreign_key "courses", "materials"
-  add_foreign_key "courses", "users"
-  add_foreign_key "levels", "users"
-  add_foreign_key "materials", "users"
 end
