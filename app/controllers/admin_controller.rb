5595df9e0ca0689de18551a72df5963d9cbb7db4
kamkara <lnclass.kamkara@gmail.com>
2021-09-02 21:01:26 +0000

    add admin

---
+++ app/controllers/admin_controller.rb
@@ -0,0 +1,13 @@
+class AdminController < ApplicationController
+  def create
+  end
+  def index
+  end
+  
+  def dashboard
+
+  end
+  def table
+
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+  get 'admin/table'
+  get 'admin/create'
+  get 'admin/index'
+  get 'admin/dashboard'
   devise_for :users
   root to:'home#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
